e3210d4491f4e7768211decaa0aa49565aa6419d
James <10becja@gmail.com>
2015-02-25 22:10:16 -0600

    Several Fixes and Additions
    Updated to 1.8
    +newPlayerProtection: hides newcomers from compass
    +option to shout across servers
    +changed configuration setup to by dynamic
    +ability to reload config

--- src/me/becja10/HardcoreSurvival/Main.java
+++ src/me/becja10/HardcoreSurvival/Main.java
@@ -1,5 +1,9 @@
 package me.becja10.HardcoreSurvival;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 import me.becja10.HardcoreSurvival.FileManager;
@@ -11,6 +15,8 @@ import org.bukkit.Material;
 import org.bukkit.World.Environment;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -18,6 +24,8 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerItemHeldEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.PluginDescriptionFile;
@@ -27,34 +35,73 @@ public class Main extends JavaPlugin implements Listener
 {
 	public final Logger logger = Logger.getLogger("Minecraft");
 	private static Main plugin;
+	
 	private static double FAR = 5000;
-	private static String banMsg = "You've died! Better luck next time.";
-	private static String msgColor = "\u00A7f";
-	private static String coordsColor = "\u00A7f";
+	private static String prefix = "\u00A7";
+	private static List<String> newbies = new ArrayList<String>();
+	
+	//config stuff	
+	public String msgColor;						//Death message color
+	public String banMsg;						//The message to display when a player is banned
+	public String coordsColor;					//customize coords color
+	public boolean newPlayerProtection;			//Whether or not to protect newbies
+	public int newbieTimer;						//How long a new player is on the newbie list
+	public boolean announce;					//if there are other servers we should announce to
 	
 	public static JavaPlugin getInstance() {return plugin;}
+		
+	private void loadConfig()
+	{
+		String configPath = plugin.getDataFolder().getAbsolutePath() + File.separator + "config.yml";
+
+		//input
+		FileConfiguration config = YamlConfiguration.loadConfiguration(new File(configPath));
+        //output
+		FileConfiguration outConfig = new YamlConfiguration();
+        
+        //load config, creating defaults if they don't exist
+        msgColor = config.getString("msgColor", "f");
+        banMsg = config.getString("banMsg", "You've died! Better luck next time.");
+        coordsColor = config.getString("coordsColor", "f");
+        newPlayerProtection = config.getBoolean("newPlayerProtection", true);
+        newbieTimer = config.getInt("newbieTimer", 30);
+        announce = config.getBoolean("announce", false);
+        
+        //write to output file
+        outConfig.set("msgColor", msgColor);
+        outConfig.set("banMsg", banMsg);
+        outConfig.set("coordsColor", coordsColor);
+        outConfig.set("newPlayerProtection", newPlayerProtection);
+        outConfig.set("newbieTimer", newbieTimer);
+        outConfig.set("announce", announce);
+        
+        try
+        {
+            outConfig.save(configPath);
+        }
+        catch(IOException exception)
+        {
+            logger.info("Unable to write to the configuration file at \"" + configPath + "\"");
+        }
+	}
 
 	@Override
 	public void onDisable()
 	{
 		PluginDescriptionFile pdfFile = this.getDescription();
-		this.logger.info(pdfFile.getName() + " Has Been Disabled!");
+		logger.info(pdfFile.getName() + " Has Been Disabled!");
 	}
 	
 	@Override
 	public void onEnable()
 	{
 		PluginDescriptionFile pdfFile = this.getDescription();
-		this.logger.info(pdfFile.getName() + " Version "+ pdfFile.getVersion() + " Has Been Enabled!");
+		logger.info(pdfFile.getName() + " Version "+ pdfFile.getVersion() + " Has Been Enabled!");
 	    getServer().getPluginManager().registerEvents(this, this); //register events
 		plugin = this; 
 		//save file
-	    saveDefaultConfig(); //fail silently if config already exists
+		loadConfig();
 	    FileManager.saveDefaultPlayers();
-	    
-		msgColor = '\u00A7'+getConfig().getString("msgColor");
-		coordsColor = '\u00A7'+getConfig().getString("coordsColor");
-
 	}
 	
 	
@@ -64,11 +111,18 @@ public class Main extends JavaPlugin implements Listener
 		//for GP, erase all claims so people can loot them
 		Player p = event.getEntity();
 		p.performCommand("abandonallclaims");
+		//announce to all servers with the listener plugin.
+		if(announce)
+		{
+			plugin.getServer().dispatchCommand(plugin.getServer().getConsoleSender(),
+							"sync console all hardcoresurvivalannounce "+p.getName());
+		}
 		//let everyone know where the person died
-		event.setDeathMessage(msgColor + event.getDeathMessage()+" at"+coordsColor +
+		event.setDeathMessage(prefix+msgColor + event.getDeathMessage()+" at"+prefix+coordsColor +
 														 " x: " + p.getLocation().getBlockX() + 
 														 " y: " + p.getLocation().getBlockY() +
 														 " z: " + p.getLocation().getBlockZ());
+		
 	}
 	
 	@EventHandler
@@ -77,7 +131,41 @@ public class Main extends JavaPlugin implements Listener
 		//in case someone needs to rejoin.
 		if(event.getPlayer().hasPermission("hardcore.noban")) return;
 		//ban the player when they respawn.
-		getServer().dispatchCommand(getServer().getConsoleSender(), "ban "+event.getPlayer().getName()+" "+banMsg);
+		plugin.getServer().dispatchCommand(getServer().getConsoleSender(), "ban "+event.getPlayer().getName()+" "+banMsg);
+	}
+	
+	@EventHandler
+	public void onLeave(PlayerQuitEvent event)
+	{
+		//remove newbie protection on log out
+		//also safeguards incase runnable screws up
+		if(newbies.contains(event.getPlayer().getName()))
+				newbies.remove(event.getPlayer().getName());
+	}
+	
+	@EventHandler
+	public void onJoin(PlayerJoinEvent event)
+	{
+		if(getConfig().getBoolean("newPlayerProtection"))
+		{
+			String id = event.getPlayer().getUniqueId().toString();
+			//if they are a new player, give them protection
+			if(!FileManager.getPlayers().contains(id))
+			{
+				FileManager.getPlayers().set(id+".name", event.getPlayer().getName());
+				FileManager.savePlayers();
+				final String name = event.getPlayer().getName();
+				newbies.add(name);
+				//schedule task to remove them from newbie list after so long
+				Bukkit.getScheduler().scheduleSyncDelayedTask(this,	new Runnable()
+				{
+					public void run()
+					{
+						newbies.remove(name); //remove them from the newbie list
+					}
+				}, 20L * getConfig().getInt("newbieTimer") * 60); //number of minutes before they are removed.
+			}	
+		}
 	}
 	
 	//Tell players how to use the compass
@@ -124,14 +212,19 @@ public class Main extends JavaPlugin implements Listener
 			
 			if(event.getAction() == Action.RIGHT_CLICK_AIR)
 			{
+				if(newbies.contains(p.getName()))
+				{
+					p.sendMessage(ChatColor.GOLD+"Your compass doesn't work yet.");
+					return;
+				}
 				Location here = p.getLocation();
 				Location closest = null; //for error checking
 				double farthest = FAR;
 				//loop through all online players
 				for(Player t : Bukkit.getOnlinePlayers())
 				{
-					//allows for some people to not be tracked, and don't track self
-					if(t.hasPermission("hardcore.notrack") || t.equals(p)) continue;
+					//allows for some people to not be tracked, don't track self, and hide newbies
+					if(t.hasPermission("hardcore.notrack") || t.equals(p) || newbies.contains(t.getName())) continue;
 					
 					//don't try and track across worlds
 					if(p.getWorld() != t.getWorld()) continue;
@@ -159,7 +252,7 @@ public class Main extends JavaPlugin implements Listener
 			{	
 				String id = p.getUniqueId().toString();
 				//if the player has stored their home
-				if(FileManager.getPlayers().contains(id))
+				if(FileManager.getPlayers().contains(id+".x"))
 				{
 					Location home = new Location(p.getWorld(),
 												FileManager.getPlayers().getDouble(id+".x"),
@@ -183,8 +276,20 @@ public class Main extends JavaPlugin implements Listener
 	//commands to set your base
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args)
 	{
+		//hardcorereload
+		if(cmd.getName().equalsIgnoreCase("hardcorereload"))
+		{
+			//if is player, and doesn't have permission
+			if((sender instanceof Player) && !(sender.hasPermission("hardcore.reload")))
+					sender.sendMessage(ChatColor.DARK_RED+"No permission.");
+			else
+			{
+				FileManager.reloadPlayers();
+				loadConfig();
+			}
+		}
 		//setbase
-		if(cmd.getName().equalsIgnoreCase("setbase"))
+		else if(cmd.getName().equalsIgnoreCase("setbase"))
 		{
 			if (!(sender instanceof Player))
 				sender.sendMessage("This command can only be run by a player.");
@@ -206,8 +311,7 @@ public class Main extends JavaPlugin implements Listener
 					p.sendMessage(ChatColor.GOLD+"Base set!");
 				}
 			}
-			return true;		
 		}
-		return false;
+		return true;
 	}
 }
